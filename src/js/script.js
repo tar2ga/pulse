4fa756dc3db7919d888611190a3752429e09c11f
tar2ga <targ2a@ya.ru>
2021-01-22 17:41:46 +0300

    урок 55 с дз

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,38 @@
-$(document).ready(function(){
-	$('.carousel__inner').slick({
-		speed: 1200,
-		//adaptiveHeight: true,
-		// autoplay: true,
-		// autoplaySpeed: 2000,
-		//пути надо прописывать с учётом того что мы как бы находимся в index.html (без ../)
-		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"</button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"</button>',
-		responsive: [
-			{
-				breakpoint: 992,
-				settings: {
-				  dots: true,
-				  arrows: false
-				}
-			}
-		]
-	});
+// $(document).ready(function(){
+// 	$('.carousel__inner').slick({
+// 		speed: 1200,
+// 		//adaptiveHeight: true,
+// 		// autoplay: true,
+// 		// autoplaySpeed: 2000,
+// 		//пути надо прописывать с учётом того что мы как бы находимся в index.html (без ../)
+// 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+// 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+// 		responsive: [
+// 			{
+// 				breakpoint: 992,
+// 				settings: {
+// 				  dots: true,
+// 				  arrows: false
+// 				}
+// 			}
+// 		]
+// 	});
+//   });
+
+//tiny-slider
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+	autoplay: false,
+	controls: false,
+	nav: false,
   });
+
+document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+});
